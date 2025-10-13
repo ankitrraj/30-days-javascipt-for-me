2853d5cddabaf03a593707288edd16698b13c73e
ankitrraj <ankitrajoria81@gmail.com>
2025-10-13 23:18:54 +0530

    the beginner freindly first code

---
+++ 30-days-javascipt/day1.js
@@ -0,0 +1,16 @@
+//  now we starts with the vairables 
+
+
+
+let name="Ankit"
+const ChildNAme = "ankit"
+let patanahi ;
+
+
+name="ram ram"
+
+
+console.log(name)
+console.log(ChildNAme)
+console.table([name,ChildNAme,patanahi])
+
