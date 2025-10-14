1c623a9ce6668c30a22d08bdd6cef31e37400404
ankitrraj <ankitrajoria81@gmail.com>
2025-10-14 23:25:56 +0530

    converstions

---
+++ 30-days-javascipt/day2.js
@@ -0,0 +1,36 @@
+// let age = "18abc"
+
+// console.log(typeof(age));
+// console.log(typeof age);
+
+// let checkThenumber = Number(age)
+// console.log( typeof checkThenumber);
+// console.log(checkThenumber);
+
+
+// let age = "Ankit"
+
+// console.log(typeof(age));
+// console.log(typeof age);
+
+// let checkThenumber = Number(age)
+// console.log( typeof checkThenumber);
+// console.log(checkThenumber);
+
+/*
+oputput
+
+object
+object
+number
+0
+
+*/
+
+let isLoggedIn =""
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+console.log(booleanIsLoggedIn);
+
+ 
+ 
---
+++ 30-days-javascipt/tempCodeRunnerFile.js
@@ -0,0 +1,8 @@
+let age = "Ankit"
+
+// console.log(typeof(age));
+// console.log(typeof age);
+
+// let checkThenumber = Number(age)
+// console.log( typeof checkThenumber);
+// console.log(checkThenumber);
