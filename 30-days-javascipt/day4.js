2aaaff72a2e60f501f12bbb7290667dd064411ec
ankitrraj <ankitrajoria81@gmail.com>
2025-10-16 23:32:37 +0530

    some changes

--- 30-days-javascipt/day4.js
+++ 30-days-javascipt/day4.js
@@ -1,3 +1,15 @@
 import {users} from './MOCK_DATA.json'
 
 console.log(users);
+
+ // we can also use the import statement to import the data from the json file
+
+ import users from './MOCK_DATA.json'
+
+ console.log(users);
+
+ // we can also use the import statement to import the data from the json file
+
+ import users from './MOCK_DATA.json'
+
+ console.log(users);
