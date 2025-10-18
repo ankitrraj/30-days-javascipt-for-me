d3d5c708ceb229b0d624a9f820361157193057df
ankitrraj <ankitrajoria81@gmail.com>
2025-10-18 11:03:21 +0530

    day5 for js

---
+++ 30-days-javascipt/day5.js
@@ -0,0 +1,19 @@
+// comparsion 
+
+// console.log("2 ">1);
+// console.log(2>1);
+
+
+// console.log(null >0);
+// console.log(null ==0);
+// console.log(null >=0);
+
+// console.log(undefined>0);
+// console.log(undefined==0);
+// console.log(undefined >=0);
+console.log("2">true);
+console.log("2">false);
+
+
+
+
