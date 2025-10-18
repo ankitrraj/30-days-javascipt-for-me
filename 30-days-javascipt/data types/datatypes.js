224583ef4d1db113c3eb02ad7ca8bf3799939a87
ankitrraj <ankitrajoria81@gmail.com>
2025-10-18 15:39:04 +0530

    data types

---
+++ 30-days-javascipt/data types/datatypes.js
@@ -0,0 +1,17 @@
+ // strings ,Number,Bollean,Null, undefined ,symbol,BigInt
+
+ const name = "ankit"
+ const num= Number(89)
+ const userLoggedIn = true
+ let OutsideTemp = null;
+ let userEmail;
+
+ const id = Symbol('123')
+ const anotherId = Symbol('123')
+console.log(id === anotherId);
+
+const BigNumber = 333454562546465347347523453453425364;
+console.log(typeof BigNumber); // its always represented as number 
+
+
+
