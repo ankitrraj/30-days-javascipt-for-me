085578aa1b78080c30a253034a505518435ed33c
ankitrraj <ankitrajoria81@gmail.com>
2025-10-21 22:59:05 +0530

    number and maths

---
+++ 30-days-javascipt/data types/Number-and-maths.js
@@ -0,0 +1,29 @@
+const newnum  = 4500000
+const balance = new Number(5500);
+
+const gold = 55.674345
+const rohit = "ankit"
+
+console.log(newnum);
+console.log(balance);
+
+console.log(balance.toFixed(4));
+
+console.log(gold.toFixed(3));
+
+console.log(gold.toString());
+
+console.log(gold.toPrecision(1 ));  // toPrecision  is mainlly used to round of the number 
+
+console.log(newnum.toLocaleString("en-IN"));
+console.log(newnum.toLocaleString("en-US"));// for counting oney an dnumbers used tolocalestring
+
+
+
+
+
+
+
+
+
+
