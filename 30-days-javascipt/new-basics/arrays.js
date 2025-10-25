da3f26672c2930c7e8792c5fff8ed778f8131d8b
ankitrraj <ankitrajoria81@gmail.com>
2025-10-25 18:55:09 +0530

    learn arrays

---
+++ 30-days-javascipt/new-basics/arrays.js
@@ -0,0 +1,40 @@
+const ankit = ["ankit", 1, 2, 3,4, 5, ]; // instialized array
+
+// ankit.shift() // shift helps remove the first element of array
+// ankit.unshift(9, 78, 5) //the unshift add  elements in the first postition[0] of the  array
+
+/*a condition in the use of shift and the unshift in the u add more than one element in the unshhift and use the shift method to he remove only the one element in the starting not the whole unshift elements */
+
+
+console.log(ankit);
+// ankit.push(4,74,84,"ankit","rohit") // add the elemnts in the array at the last postition
+// ankit.pop() //he remove the element at the last postition 
+
+// console.log(ankit);
+// console.log(ankit.includes(9))
+// console.log(ankit.indexOf("ankti"));
+let ankti = new Array(4,52,5,35,5,22,2,64);
+
+// console.log(ankit);
+let newArr = ankti.sort((a,b )=> a-b) //nnumeric sort 
+
+
+console.log(newArr);
+
+
+console.log(ankit);
+
+// console.log(newArr);
+// console.log(typeof newArr);
+// console.log(typeof ankit);
+
+
+
+
+
+
+
+
+
+
+
