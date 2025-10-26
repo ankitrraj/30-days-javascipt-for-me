9c44a98645f26ef6524dd573eb6d521799e96a8a
ankitrraj <ankitrajoria81@gmail.com>
2025-10-26 22:23:46 +0530

    objects day 1

---
+++ 30-days-javascipt/objects/objects-1.js
@@ -0,0 +1,34 @@
+
+
+const mysymbol = Symbol("mysym")
+// console.log(typeof mysymbol);
+
+const myobj = {
+    name : "Ankit",
+    age : 18,
+    class : "BCA 2nd Year ",
+    email : "ankitrajoria@gmail.com",
+    "full name" : "ankit rajoria",
+    isLoggedIn : true,
+    LastLoginDays : [ "monday ", "tuesday ", "saturday"],
+    // [mysymbol] : "mysym",
+    //when you try to know this mysymbol type to its output is a string because you have demand to typeof the value in behind the seen he like this mysymbol(mysym ): "mysym" 
+    // when you need to the output is symbol to the value is always a symbol
+    [mysymbol] :Symbol("mysymbolisnow")
+
+}
+
+console.log(myobj.name);
+//and a another beat approach to get the values in the obj 
+console.log(myobj["name"]);
+console.log(myobj["email"]);
+console.log(myobj["full name"]);// and a another thing when u delcare a obj value in " " codes to iunsure the all the spaces count in the "" codes
+
+console.log(typeof myobj[mysymbol]);
+console.log(myobj[mysymbol]);
+
+
+
+
+
+
