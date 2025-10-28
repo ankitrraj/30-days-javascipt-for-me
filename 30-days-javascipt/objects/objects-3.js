3fe5496c39233622a8649551e582d9bef8bb9c76
ankitrraj <ankitrajoria81@gmail.com>
2025-10-28 22:37:32 +0530

    a method in js

---
+++ 30-days-javascipt/objects/objects-3.js
@@ -0,0 +1,11 @@
+// destructor in js 
+
+const myobj = {
+    name : "ankit",
+    ok : "notokay"
+}
+
+const {name:th} = myobj //that is coorect 
+const {name} = myobj // that is also correct 
+console.log(th);
+
