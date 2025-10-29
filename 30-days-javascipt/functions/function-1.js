1222e736b956f026d706fdceb2fd71f8e78caffd
ankitrraj <ankitrajoria81@gmail.com>
2025-10-29 22:45:14 +0530

    function in js

---
+++ 30-days-javascipt/functions/function-1.js
@@ -0,0 +1,20 @@
+// functions
+function myfunc(a,b) {
+    console.log(a+b)
+}
+// myfunc(2,"4")
+
+
+function islogged(username){
+
+    if(username === undefined){
+        console.log("please enter a username");
+        
+        
+
+    }else{
+    return `${username} ne login kar liya`
+    }
+}
+
+console.log(islogged(""));
