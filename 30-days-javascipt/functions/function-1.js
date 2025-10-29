22f5d6b16d29f342c0b2c60431a9c4483f5a0729
ankitrraj <ankitrajoria81@gmail.com>
2025-10-29 22:49:16 +0530

    function part 1 complete

--- 30-days-javascipt/functions/function-1.js
+++ 30-days-javascipt/functions/function-1.js
@@ -1,13 +1,21 @@
 // functions
+// function myfunc(a,b) {
+//     console.log(a+b)
+// }
+
+
 function myfunc(a,b) {
-    console.log(a+b)
+    return a+b;
 }
+
+console.log(myfunc(4,98));
+
 // myfunc(2,"4")
 
 
-function islogged(username){
+function islogged(username){// you have delcare the default value
 
-    if(username === undefined){
+    if(username === undefined){ // you alswo use !username
         console.log("please enter a username");
         
         
