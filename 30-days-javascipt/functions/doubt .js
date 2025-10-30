2b35a63f02c0cb8a32852ca878bf5f03c758787e
ankitrraj <ankitrajoria81@gmail.com>
2025-10-30 21:28:39 +0530

    built in function

---
+++ 30-days-javascipt/functions/doubt .js
@@ -0,0 +1,20 @@
+let num1 = prompt("enter the value of num1");
+let num2 = prompt("enter the value of num2");
+
+
+num1= num1 + num2 //16
+console.log(num1 ,num2);
+
+num2= num1-num2
+console.log(num1 ,num2);
+num1 = num1 -num2
+console.log(num1 ,num2);
+
+
+
+
+
+ 
+
+console.log(num1);
+console.log(num2);
--- 30-days-javascipt/functions/function-1.js
+++ 30-days-javascipt/functions/function-1.js
@@ -26,3 +26,26 @@ function islogged(username){// you have delcare the default value
 }
 
 console.log(islogged(""));
+
+
+function swaptwonum(num1,num2){
+
+    console.log(`before num-1 is ${num1} and num-2 is ${num2}`);
+    
+   num1 = num1 + num2
+   num2= num1-num2
+   num1 = num1-num2
+    console.log(`after swap the num1 is ${num1}  and num2 is ${num2}`);
+    
+    
+}
+
+
+// console.log("hdu")
+let num1 = prompt("enter the value of num1"); // the prompt is only using in web 
+let num2 = prompt("enter the value of num2");
+// in the terminal and compiler u use the readline
+ 
+
+swaptwonum(num1 ,num2)
+
