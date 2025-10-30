99e477e75bb790d5034cb21d4148e040fe0965ae
ankitrraj <ankitrajoria81@gmail.com>
2025-10-30 23:45:39 +0530

    function_objects

---
+++ 30-days-javascipt/functions/function_with_objects.js
@@ -0,0 +1,50 @@
+// function calculateCartPrice(...num1){
+//     return num1;
+
+// }
+
+// function calculateCartPrice(...num1){
+//     return num1;
+
+// }
+// console.log(calculateCartPrice(200,15,0,15,48));
+//output  [ 200, 15, 0, 15, 48 ]
+function calculateCartPrice(vaal,val2 ,...num1){
+    return num1;
+
+}
+console.log(calculateCartPrice(200,15,0,15,48));
+
+// [ 0, 15, 48 ] the vaal it has => 200 and val2 has=>15
+
+const user ={
+    name : "ankit",
+    surname : " rajoria"
+}
+
+// function myuserobjfunc(anyobject){
+//     console.log(`my name is ${anyobject.name}  and my surname is${anyobject.surname}`);
+    
+// }
+// myuserobjfunc(user)
+
+// iuse a another method also
+
+function myuserobjfunc(anyobject){
+    console.log(`my name is ${anyobject.name}  and my surname is${anyobject.surname}`);
+    
+}
+myuserobjfunc({
+    name : "rohit",
+    surname : "asnkdfu"
+})
+
+const newaraay = [100,2000,300,600]
+
+function returnvalue(getArray){
+    return getArray[0]
+
+}
+// console.log(returnvalue(newaraay));
+ console.log(returnvalue([100,45,455,4784,4]));
+ 
