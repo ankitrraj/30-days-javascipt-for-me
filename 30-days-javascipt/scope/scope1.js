85bb8ba86d0d70d4e3e7e67e4ab57aa620d4f971
ankitrraj <ankitrajoria81@gmail.com>
2025-10-31 23:10:26 +0530

    scopes and nested

--- 30-days-javascipt/scope/scope1.js
+++ 30-days-javascipt/scope/scope1.js
@@ -8,7 +8,49 @@ if ( true ) {
     const b =456;
      c =789;
 }
-
+// {} that is the scope
 // console.log(a);
 // console.log(b);
 console.log(c);// because the c is givern output of the block scope its a problem
+
+
+// +++++++++++++++++++++++++++++++++++++++++++++++
+
+function okbyebye(){
+    const username = "bhes"
+    function nahibhaiye(){
+        const meranaam = "Ankit"
+        console.log(`my buffalo name is ${username}`);
+        
+    }
+    // because is not in the scope 
+    // console.log(`my name is the ${meranaam}`);
+    nahibhaiye()
+    
+}
+okbyebye()
+
+
+if( true){
+    const username = "bhes"
+    if(username === "bhes"){
+        const web = "newsdikhao.co.in"
+        console.log(`my buffalo name is ${username}`);
+        
+    }
+    // console.log(web);
+    
+
+}
+// console.log(username);
+
+console.log(addone(5));
+
+function addone(num){
+    return num +1;
+}
+// its call the delcration of function and get 
+name(5)
+const name = function addone(num){
+    return num +3;
+}
