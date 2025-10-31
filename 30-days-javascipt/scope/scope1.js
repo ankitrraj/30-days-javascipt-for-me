8a53fa66740082772750f1d088bf277825cbf8c8
ankitrraj <ankitrajoria81@gmail.com>
2025-10-31 22:05:49 +0530

    socpes in js

---
+++ 30-days-javascipt/scope/scope1.js
@@ -0,0 +1,14 @@
+// let a =123;
+// const b =456;
+var c =215;
+
+if ( true ) {
+    
+    let a =123;
+    const b =456;
+     c =789;
+}
+
+// console.log(a);
+// console.log(b);
+console.log(c);// because the c is givern output of the block scope its a problem
