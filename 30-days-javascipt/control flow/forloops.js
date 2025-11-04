2b5c1fed4974886a8c98e2eace439fe05045fc97
ankitrraj <ankitrajoria81@gmail.com>
2025-11-05 00:08:12 +0530

    for loops complete

---
+++ 30-days-javascipt/control flow/forloops.js
@@ -0,0 +1,32 @@
+for (let i = 0; i <= 10; i++) {
+    if(i ==5){
+        console.log(`detected its 5 `);
+        
+        continue;
+    }
+    console.log(i);
+    
+    
+}
+
+// for (let i = 1; i < 10; i++) {
+//     console.log(`its outer loop ${i}`);
+    
+//     for (let j = 1; j < 10; j++) {
+//         // console.log(`its inner loop ${j} and outer is ${i} `);
+//         console.log(`${i} * ${j} = ${i*j}`);
+        
+        
+//     }
+    
+// }
+
+// let myherosarray = ["panduram" , "bhes" ,"GOAT"]
+// for (let index = 0; index < myherosarray.length; index++) {
+//     const element = myherosarray[index];
+//     console.log(element);
+    
+    
+// }
+
+// break and continue 
