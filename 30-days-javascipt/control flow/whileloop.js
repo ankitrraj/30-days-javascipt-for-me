3db5982f8333582cbaabcf6d8c256c473282f24f
ankitrraj <ankitrajoria81@gmail.com>
2025-11-05 23:35:16 +0530

    while or do while

---
+++ 30-days-javascipt/control flow/whileloop.js
@@ -0,0 +1,25 @@
+// while loop 
+let i = 0;
+// while (i<=10) {
+//     console.log(`value of i is ${i}`);
+//     i =i+2
+
+
+// }
+let myherosarray = ["sher", "sheru1", "sheru2"]
+//                   0      1         2
+
+let arr = 0
+while (arr < myherosarray.length) { // 3
+    console.log(`value of i is ${myherosarray[arr]}`);//myherosarray[arr] its use to print the value one by one 
+    // when you dont use [arr ] to its has no go to second value he prints all the value at same time
+    arr++
+
+
+}
+let score =1;
+do {
+    console.log(`score is ${score}`);
+    score++
+    
+} while (score<=14);
