b224b035a2fb72e97269dd0fc4d26adadcfef5f6
ankitrraj <ankitrajoria81@gmail.com>
2025-11-06 23:04:05 +0530

    high order loops

---
+++ 30-days-javascipt/control flow/high_order_array_loops.js
@@ -0,0 +1,45 @@
+// now we talk about to the for of loop
+
+// const ankit = "ankit rajoria"
+
+// for (const i of ankit) {
+//     console.log(i);
+    
+    
+// }
+
+// const ankit = "ankit rajoria"
+
+// for (const i of ankit) {
+//     if(i  ===" ")
+//         break;
+//     console.log(i);
+    
+    
+// }
+
+// const map = new Map()
+// map.set("ankit" ,"rohit");
+// map.set("9027" ,"9019");
+// map.set("classes" ,"BCA2NDYEAR");
+
+// // console.log(map);
+
+// for (const [key,value] of map) {
+//     console.log(key,value);
+        
+// }
+
+const myobj = {
+     rohit : "ankit",
+    class : "bca2nd"
+}
+
+for (const i of myobj) {
+    console.log(i);
+    
+    
+}
+/// the obj is not working 
+
+
