6d18c77ff72e0e835ec0ea9cbb6efb941370502d
ankitrraj <ankitrajoria81@gmail.com>
2025-11-07 22:58:39 +0530

    foreach loops in deep

---
+++ 30-days-javascipt/control flow/foreachloop.js
@@ -0,0 +1,50 @@
+const coding = ["js","ruby","java","python"]
+
+// coding.forEach( function (val) {
+//     console.log(val);
+    
+    
+// })
+// coding.forEach( (val ) => {
+//     console.log(val);
+    
+
+// })
+
+// function printMe(item){
+//     console.log((item));
+    
+// }
+// coding.forEach(printMe)
+
+// coding.forEach( (val,index,arr ) => {
+//     console.log(val,index,arr);
+    
+
+// })
+
+// coding.forEach( (Ankit ) => { // you pass any value in paranthesses
+//     console.log(Ankit);
+    
+
+// })
+
+const mycoding= [
+    {
+        nameoflanguage : "python ",
+        mark : 'py'
+    },
+    {
+        nameoflanguage : "javascript ",
+        mark : 'js'
+    },
+    {
+        nameoflanguage : "ruby ",
+        mark : 'ruby'
+    }
+]
+
+mycoding.forEach( (item) => {
+    console.log(item.mark);
+    
+}); // when you complete there and no code after thisa to make use ; this
---
+++ 30-days-javascipt/control flow/tempCodeRunnerFile.js
@@ -0,0 +1,5 @@
+coding.forEach( (Ankit ) => { // you pass any value in paranthesses
+//     console.log(Ankit);
+    
+
+// })
