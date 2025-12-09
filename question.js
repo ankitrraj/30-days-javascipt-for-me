e0a8a4b462502cfca5c7825689f9938dae964950
ankitrraj <ankitrajoria81@gmail.com>
2025-12-09 20:01:08 +0530

    some another changes

---
+++ question.js
@@ -0,0 +1,22 @@
+// const arr = [1,2,3];
+// console.log(arr);
+
+// const newArr = [...arr]; // 
+// console.log(newArr);
+
+// function sum(...nums) {   // collect
+//   return nums;
+// }
+// console.log(sum());
+
+// sprad and check question
+
+let m = 10;
+
+function run() {
+  console.log(m);
+  let m = 20;
+}
+
+run();
+
